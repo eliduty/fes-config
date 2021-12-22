97099cc9107f75c110b0a603444014b8051e6236
eliduty <eliduty@qq.com>
2021-12-22 14:11:01 +0800

    fix: 修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,37 @@
 module.exports = {
-  parser: 'vue-eslint-parser',
+  env: {
+    browser: true,
+    node: true,
+  },
   parserOptions: {
-    parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'plugin:prettier/recommended'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/camelcase': 'off',
+    'vue/no-unused-components': 'warn',
+    'vue/multi-word-component-names': 'off',
+    'vue/script-setup-uses-vars': 'error',
+    'prefer-const': 'off',
+    'object-shorthand': 'warn',
     camelcase: ['warn', { properties: 'never' }],
     quotes: ['warn', 'single'],
     semi: 'warn',
-    'prefer-const': 'off',
     'prettier/prettier': [
       'off',
       {
-        singleQuote: true,
-        semi: true,
-        trailingComma: 'none'
-      }
-    ]
-  }
+        singleQuote: false,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+        sortAttributes: true,
+        'key-spacing': false,
+      },
+    ],
+  },
 };
