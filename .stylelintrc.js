eb39d3facce6acdbfe79bc82644dc9439887ea56
eliduty <eliduty@qq.com>
2021-12-28 16:59:11 +0800

    fix: styleLint 增加BEM支持

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
   plugins: ['stylelint-order'],
   rules: {
+    'selector-class-pattern': '^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$',
     'selector-pseudo-class-no-unknown': [
       true,
       {
