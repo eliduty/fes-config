b8529a96539613e2787e6e5ded2e4030cbdf0fbd
eliduty <eliduty@qq.com>
2022-01-10 20:48:42 +0800

    fix: 修改stylelint配置

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 // https://www.npmjs.com/package/lint-staged
 module.exports = {
   'src/**/*.{js,ts,tsx,vue}': ['prettier --write','eslint --cache --fix'],
-  'src/**/*.{vue,htm,html,css,sss,less,scss}': ['stylelint --fix --custom-syntax postcss-html'],
+  'src/**/*.{vue,htm,html,css,sss,less,scss}': ['stylelint --fix'],
 };
